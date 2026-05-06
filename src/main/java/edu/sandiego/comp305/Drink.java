ad8f25212df75d7a99e237ae3a89f71f921458c2
angiejeong <ajeong@sandiego.edu>
2026-05-05 23:53:11 -0700

    Update Drink.java

--- src/main/java/edu/sandiego/comp305/Drink.java
+++ src/main/java/edu/sandiego/comp305/Drink.java
@@ -22,7 +22,7 @@ public abstract class Drink {
 
     public String getItem() {
         if (isIced) {
-            return ("Iced " item);
+            return ("Iced " + item);
         } else {
             return item;
         }
