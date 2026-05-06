5935afbabb1cf894e44421d9692bb9d323e66777
Angie Jeong <ajeong@sandiego.edu>
2026-05-06 11:16:07 -0700

    Removed instances of Food

--- src/main/java/edu/sandiego/comp305/Order.java
+++ src/main/java/edu/sandiego/comp305/Order.java
@@ -8,12 +8,12 @@ public class Order {
     private final String customerName;
     //private final List<Drink> drinks;
 
-    private final List<Food> foods;
+    //private final List<Food> foods;
 
     public Order(final String customerName) {
         this.customerName = customerName;
         //this.drinks = new ArrayList<>();
-        this.foods = new ArrayList<>();
+        //this.foods = new ArrayList<>();
     }
 
     //public void addDrink(Drink drink) {
